dd1a99096fd326810d4b7ebb2c4013922841a5db
Tudubum <gdeveikyte95@gmail.com>
2023-05-05 17:32:33 +0300

    added comments

--- index.js
+++ index.js
@@ -21,18 +21,23 @@ let colors = [
     '#fdf9a1',
   ];
 
-
 let lastElement = null; //The variable lastElement will be used as a storage location to hold the value of the last element.
 
 function GetRandom(max){ // why we use max?
     return parseInt(Math.floor(Math.random() * max));
 }
 
+function GetRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  
 function CreateElement(){
     lastElement = document.createElement('div');
     lastElement.setAttribute('class','shape');
     lastElement.style.position = 'absolute';  // without absolute shape won't move
     lastElement.style.top = "60px";
+    lastElement.style.left = GetRandomInRange(300, 700) + "px"
     lastElement.style.width = "50px";
     lastElement.style.height = "50px";
     lastElement.style.backgroundColor = colors[GetRandom(colors.length)];
@@ -40,13 +45,20 @@ function CreateElement(){
     grid.appendChild(lastElement);
 }
 
-// WHY WE USE parseInt?
+// WHY HERE WE USE parseInt?
+
+//The main purpose of using the parseInt function is to extract a number from a string. This turns the returned value to an actual number
+
 
 function MoveLastElement(){
-    let yPosition = lastElement.style.top;
-    let yPositionInt = parseInt(yPosition);
+    let yPosition = lastElement.style.top; //funkcija nuskaito paskutinio elemento viršutinę koordinatę (yPosition)
 
-    yPositionInt = yPositionInt + 10;
+    let yPositionInt = parseInt(yPosition);
+    //he variable yPosition is converted from a string to an integer using the parseInt() function, and the result is stored in the variable yPositionInt
+
+    yPositionInt = yPositionInt + 10; //why + 10?
+    //10 is added to the value of yPositionInt to move the last element down. 
+
     lastElement.style.top = yPositionInt + "px";
 
     if (yPositionInt > 620){    
